45be34a537da321c8861b95fb5b9ae1e9d3b844a
Michal Wronski <michal.robert.wronski@gmail.com>
2026-07-16 18:29:49 +0200

    resume - progress bar fixes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,14 @@ import { createRouter, createWebHistory, type RouterScrollBehavior } from 'vue-r
 import Home from '../pages/Home.vue'
 import Resume from '../pages/Resume.vue'
 
-const scrollBehavior: RouterScrollBehavior = (to, _from, savedPosition) => {
+const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
   if (savedPosition) return savedPosition
   if (to.hash) {
     return { el: to.hash, top: 80, behavior: 'smooth' }
+  }
+  // Always reset scroll on route change (avoids progress nav reading leftover scrollY).
+  if (to.path !== from.path) {
+    return { top: 0, left: 0 }
   }
   return { top: 0 }
 }
