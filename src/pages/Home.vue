d55b72fd308e966103e2f2fbf1e1e3c4664b341b
Michal Wronski <michal.robert.wronski@gmail.com>
2026-07-16 11:22:43 +0200

    main page polishing

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,8 +16,8 @@
           </h1>
           <p class="mt-4 text-lg text-slate-700">{{ $t('about.pitch') }}</p>
           <div class="mt-8 flex flex-wrap items-center gap-3">
-            <a href="mailto:contact@bigmikesolutions.pl" class="btn">Work with me</a>
-            <RouterLink to="/#services" class="btn btn--ghost">View services</RouterLink>
+            <a href="mailto:contact@bigmikesolutions.pl" class="btn">{{ $t('about.buttons.work') }}</a>
+            <RouterLink to="/#services" class="btn btn--ghost">{{ $t('about.buttons.view') }}</RouterLink>
             <a
               href="https://github.com/m-wrona"
               target="_blank"
@@ -35,7 +35,7 @@
                   d="M12 .5C5.73.5.5 5.73.5 12c0 5.08 3.29 9.38 7.86 10.9.58.11.79-.25.79-.56 0-.28-.01-1.02-.02-2-3.2.7-3.88-1.54-3.88-1.54-.53-1.36-1.3-1.72-1.3-1.72-1.06-.73.08-.71.08-.71 1.17.08 1.79 1.2 1.79 1.2 1.04 1.78 2.73 1.27 3.4.97.11-.75.41-1.27.75-1.56-2.55-.29-5.23-1.27-5.23-5.66 0-1.25.45-2.28 1.19-3.09-.12-.29-.52-1.47.11-3.07 0 0 .97-.31 3.18 1.18.92-.26 1.9-.39 2.88-.39.98 0 1.96.13 2.88.39 2.2-1.49 3.17-1.18 3.17-1.18.63 1.6.23 2.78.11 3.07.74.81 1.19 1.84 1.19 3.09 0 4.4-2.69 5.36-5.25 5.64.42.36.79 1.07.79 2.15 0 1.55-.01 2.8-.01 3.18 0 .31.21.68.8.56C20.71 21.38 24 17.08 24 12c0-6.27-5.23-11.5-12-11.5z"
                 />
               </svg>
-              <span>GitHub</span>
+              <span>{{ $t('about.buttons.github') }}</span>
             </a>
           </div>
         </div>
